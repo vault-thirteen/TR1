3c9b55990e4438cb2e2459bcf983b8d4c8172788
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2024-11-24 21:50:04 +0300

    JWT KM Test

--- src/libraries/km/KeyMaker_test.go
+++ src/libraries/km/KeyMaker_test.go
@@ -11,12 +11,14 @@ import (
 func Test_KeyMaker_ValidateToken(t *testing.T) {
 	const (
 		Common_SigningMethodName  = TokenAlg_RS512
-		Common_PrivateKeyFilePath = "cert\\JWT\\jwtPrivateKey.pem"
-		Common_PublicKeyFilePath  = "cert\\JWT\\jwtPublicKey.pem"
+		Common_PathPrefix         = `..\..\..\`
+		Common_PrivateKeyFilePath = Common_PathPrefix + "cert\\JWT\\jwtPrivateKey.pem"
+		Common_PublicKeyFilePath  = Common_PathPrefix + "cert\\JWT\\jwtPublicKey.pem"
 	)
 
 	userId := 123
 	sessionId := 456
+	var a, b int
 
 	aTest := tester.New(t)
 	n := 20_000
@@ -45,8 +47,10 @@ func Test_KeyMaker_ValidateToken(t *testing.T) {
 
 		var t1 = time.Now()
 		for i := 0; i < n; i++ {
-			_, _, err = keyMaker.ValidateToken(tokenString)
+			a, b, err = keyMaker.ValidateToken(tokenString)
 			aTest.MustBeNoError(err)
+			aTest.MustBeEqual(a, userId)
+			aTest.MustBeEqual(b, sessionId)
 		}
 		var dur = time.Now().Sub(t1)
 		resumeFN(1, dur)
@@ -70,8 +74,10 @@ func Test_KeyMaker_ValidateToken(t *testing.T) {
 
 		var t1 = time.Now()
 		for i := 0; i < n; i++ {
-			_, _, err = keyMaker.ValidateToken(tokenString)
+			a, b, err = keyMaker.ValidateToken(tokenString)
 			aTest.MustBeNoError(err)
+			aTest.MustBeEqual(a, userId)
+			aTest.MustBeEqual(b, sessionId)
 		}
 		var dur = time.Now().Sub(t1)
 		resumeFN(2, dur)
