7f465d6af32e8b499a1505c23ae577101e51b688
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2025-03-10 18:44:09 +0300

    ...

--- src/models/common/Forum.go
+++ src/models/common/Forum.go
@@ -2,9 +2,9 @@ package cm
 
 type Forum struct {
 	MetaData
-	Id      int    `json:"id" gorm:"primarykey"`
-	Name    string `json:"name" gorm:"uniqueIndex,size:255"`
-	Threads []Thread
+	Id      int      `json:"id" gorm:"primarykey"`
+	Name    string   `json:"name" gorm:"uniqueIndex,size:255"`
+	Threads []Thread `json:"threads,omitempty,omitzero"`
 
 	// Forum's position in the list of all forums.
 	// 1 = top position; the greater number is, the lower the position is.
